d290123d89a04a0f1ae67ac548b7dcade5dc3a44
naoya <t_naoya_k@yahoo.co.jp>
2019-05-10 15:11:40 +0900

    Update display_group-name_leftbar_html_haml

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,21 @@
 class GroupsController < ApplicationController
 
-  def new
+  def index
 
   end
 
-  def create
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
+  def create
+    group = Group.new(group_params)
+    if group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,7 +23,12 @@ class GroupsController < ApplicationController
   end
 
   def update
-    
+
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
 end
