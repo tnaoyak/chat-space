6650ab0d00f34a1980747392c1e1c81cecff61e1
naoya <t_naoya_k@yahoo.co.jp>
2019-05-10 17:51:32 +0900

    Update group edit.html.haml

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
 
@@ -23,12 +24,20 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    if @group.update(group_params)
+      redirect_to group_message_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[id])
   end
 
 end
