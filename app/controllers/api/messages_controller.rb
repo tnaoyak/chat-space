6ecea8cc2b871310240b449896d15a7b8c820cbb
naoya <t_naoya_k@yahoo.co.jp>
2019-05-22 11:44:45 +0900

    Auto_update_implementation

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,11 @@
+class Api::MessagesController < ApplicationController
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { @messages = Message.where('id > ?', params[:id]) }
+    end
+    
+  end
+
+end
