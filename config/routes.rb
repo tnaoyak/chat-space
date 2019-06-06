431d7a3245f00b92ade182f9508f98e3ce4dcad0
naoya <t_naoya_k@yahoo.co.jp>
2019-06-06 18:44:19 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -22,19 +22,3 @@ resources :users do
     get 'registration_complete'
   end
 end
-class UsersController < ApplicationController
-  def login
-  end
-  def signup
-  end
-  def user_registration
-  end
-  def sms_confirmation
-  end
-  def address_registration
-  end
-  def card_registration
-  end
-  def registration_complete
-  end
-end
